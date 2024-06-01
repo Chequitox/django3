dc7df9d8da94c69de16646c18d5ec1b7c8f7476c
Checo <joakingera@gmail.com>
2024-06-01 17:39:25 -0400

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Post
+
+# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+    model = Post
